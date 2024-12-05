bebfe7862eca3500cb218354fb2f776af4522301
Vitaly <vitaliy.zayakin@gmail.com>
2024-12-06 02:17:56 +0500

    feat: pass 7 test cases

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -199,8 +199,14 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let result = '';
+
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    result += str[i];
+  }
+
+  return result === str;
 }
 
 /**
@@ -217,8 +223,14 @@ function isPalindrome(/* str */) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      return i;
+    }
+  }
+
+  return -1;
 }
 
 /**
@@ -236,8 +248,19 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let number = num;
+  let flag = true;
+
+  while (flag && number >= 1) {
+    if (number % 10 === digit) {
+      flag = false;
+    }
+
+    number = Math.floor(number / 10);
+  }
+
+  return !flag;
 }
 
 /**
