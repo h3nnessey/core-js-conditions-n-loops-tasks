46683f595abdd1ff319c34cee7a4c2d65c1d7c13
Vitaly <vitaliy.zayakin@gmail.com>
2024-12-20 15:54:17 +0500

    feat: pass 14 test cases

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -428,8 +428,25 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let result = str;
+
+  for (let i = 0; i < iterations; i += 1) {
+    let start = '';
+    let end = '';
+
+    for (let j = 0; j < result.length; j += 1) {
+      if (j % 2 === 0) {
+        start += result[j];
+      } else {
+        end += result[j];
+      }
+    }
+
+    result = start + end;
+  }
+
+  return result;
 }
 
 /**
